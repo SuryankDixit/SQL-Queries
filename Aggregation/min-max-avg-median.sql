8554a6387ce8724eb531662a8ec73f6a7d9c2ed2
suryankdixit <suryankdixit@gmail.com>
2021-04-23 11:54:38 +0530

    https://classroom.udacity.com/courses/ud198/lessons/76a484da-1f2e-4886-ba2c-684bb30e267d/concepts/8f65cc46-cd5b-4da4-b648-24313ff8a560

---
+++ Aggregation/min-max-avg-median.sql
@@ -0,0 +1,13 @@
+-- When was the earliest order ever placed?
+SELECT MIN(occurred_at) 
+FROM orders;
+
+-- When did the most recent (latest) web_event occur?
+SELECT MAX(occurred_at)
+FROM web_events;
+
+-- Find the mean (AVERAGE) amount spent per order on each paper type, as well as the mean amount of each paper type purchased per order. Your final answer should have 6 values - one for each paper type for the average number of sales, as well as the average amount.
+SELECT AVG(standard_qty) mean_standard, AVG(gloss_qty) mean_gloss, 
+           AVG(poster_qty) mean_poster, AVG(standard_amt_usd) mean_standard_usd, 
+           AVG(gloss_amt_usd) mean_gloss_usd, AVG(poster_amt_usd) mean_poster_usd
+FROM orders;
